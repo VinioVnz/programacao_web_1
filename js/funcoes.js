3cff37b9a1ac6719a43ac1be0298d66d5ff79047
Vinícius <vinicius.bornhofen47@gmail.com>
2026-04-14 21:20:28 -0300

    js

---
+++ js/funcoes.js
@@ -0,0 +1,17 @@
+function minhaFuncao2(pParametro) {
+    console.log("Parametro é "+pParametro)
+}
+
+function minhaFuncao3(pParametro){
+    return pParametro*2;
+}
+
+function minhaFuncao4(){
+    x+=10;
+    var vInterna2 = "Blumenau"
+}
+
+function minhaFuncao5(){
+    var x = 100;
+    var vInterna = "Brasil"
+}
