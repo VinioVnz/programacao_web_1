b45fc48d9cc003e5711587001982ef658fcbb3be
Vinícius <vinicius.bornhofen47@gmail.com>
2026-05-12 21:04:25 -0300

    modulo e promisses

---
+++ js/modulo1.js
@@ -0,0 +1,9 @@
+export var Professor_modulo = {
+    nome: "marcos",
+    idade: 53,
+    departamento: "DSC"
+}
+
+export function olaMundo(){
+    console.log("Olá Mundo!");
+}
---
+++ js/modulo2.js
@@ -0,0 +1,10 @@
+function olaMundo2(){
+    console.log("Olá Mundo 2!");
+}
+const Aluno = {
+    nome: "vinicius",
+    idade: 19,
+    curso: "DSC"
+}
+
+export {olaMundo2, Aluno}
